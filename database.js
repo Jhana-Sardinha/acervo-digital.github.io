c9865075a41fbcc3d496dae376231ba71183ab37
Jhana Sardinha <sardinha.jhana@gmail.com>
2026-02-05 16:38:53 -0300

    Create database.js

---
+++ database.js
@@ -0,0 +1,21 @@
+const EBOOKS_DATA = [
+    { 
+        id: 1, 
+        titulo: "Front-End Pro 2026", 
+        categoria: "Tecnologia", 
+        preco: "R$ 47,90", 
+        capa: "https://images.unsplash.com/photo-1555066931-4365d14bab8c?q=80&w=1000", 
+        rotation: -3, 
+        link: "#" 
+    },
+    { 
+        id: 2, 
+        titulo: "Manual do Rebelde", 
+        categoria: "Estratégia", 
+        preco: "R$ 29,90", 
+        capa: "https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?q=80&w=1000", 
+        rotation: 2, 
+        link: "#" 
+    }
+    // O ADMIN VAI GERAR NOVOS BLOCOS PARA COLAR AQUI
+];
